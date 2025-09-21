1cc3e3a19e4b89ecab100a50b2111f7545396ff1
veronique-marro <veronique.marro@laplateforme.io>
2025-09-21 11:29:54 +0200

    Supression des anciens fichiers

--- me.php
+++
@@ -1,5 +0,0 @@
-<?php $prenom = "Veronique";
-$age = 25;
-$ville = "Marseille"; 
-
-echo "Je m'apelle $prenom j'ai $age et j'habite à $ville" ?> 
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-const  MAX_SPEED = 130;
-$speed = 140;
-
-// MAX_SPEED = 90;
-$speed = 110;
-
-echo "La vitesse maximum est de " .MAX_SPEED. " et vous rouliez à $speed";
-?>
